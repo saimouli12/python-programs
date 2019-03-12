d0f41d517df75f5cfac8cebf6b7c55b68e427d0b
saimouli12 <48007512+saimouli12@users.noreply.github.com>
2019-03-12 12:04:09 +0530

    Add files via upload

--- sai8.py
+++ sai8.py
@@ -1,9 +1,14 @@
-l=[1,2,3,4]
-def su(l):
-	s=0
-	for i in l:
-		s=s+i
-	return s
-s=su(l)
-print(s)
-
+try:
+	l=[1,2,3,4]
+	a=int(input("enter a number"))
+	if a>5:
+		raise NameError
+	else:
+		print(l)
+		raise TypeError
+except NameError:
+	print("Name error")
+except TypeError:
+	print("type error")
+finally:
+	print("successful execution")
